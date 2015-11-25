7469f8dfe5e3ba20c93bc8da25c45457cf5829a6
Isaac Hess <isaachess@gmail.com>
2015-11-25 15:05:37 -0700

    Typing

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import * as request from 'request';
 import * as fs from 'fs';
 
 var url = 'https://userstream.twitter.com/1.1/user.json'
-var oauth = {
+var oauth:request.OAuthOptions = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     token: process.env.TWITTER_ACCESS_TOKEN,
